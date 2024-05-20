941c81516417007a9d14a643ded8af5201adcf8f
Yan Christofer Silalahi <yanchristof@gmail.com>
2024-05-20 18:58:28 +0700

    feat: initiate cicd setup

---
+++ Dockerfile
@@ -0,0 +1,44 @@
+# Use the official Python image as the base image
+FROM python:3.9-slim
+
+# env variable as build arguments
+ARG DATABASE_NAME
+ARG DATABASE_USER
+ARG DATABASE_PASSWORD
+ARG DATABASE_HOST
+ARG DATABASE_PORT
+ARG SECRET_KEY
+ARG ENVIRONMENT
+
+# set env variables for docker
+ENV DATABASE_NAME=$DATABASE_NAME
+ENV DATABASE_USER=$DATABASE_USER
+ENV DATABASE_PASSWORD=$DATABASE_PASSWORD
+ENV DATABASE_HOST=$DATABASE_HOST
+ENV DATABASE_PORT=$DATABASE_PORT
+ENV SECRET_KEY=$SECRET_KEY
+ENV ENVIRONMENT=$ENVIRONMENT
+
+# Set the working directory in the container
+WORKDIR /docker-app
+
+# Copy the requirements file into the container at /docker-app
+COPY requirements.txt /docker-app/
+
+# Install any dependencies specified in requirements.txt
+RUN pip install --upgrade pip && pip install -r requirements.txt
+
+# Copy the current directory contents into the container at /docker-app
+COPY . /docker-app/
+
+# Expose port 8000
+EXPOSE 8000
+
+# Collect static for better visualization
+RUN python manage.py collectstatic
+
+# Run DB migration
+RUN python manage.py migrate
+
+# Run Gunicorn
+CMD ["gunicorn", "app.nani.wsgi", "--bind", "0.0.0.0:8000"]
--- blur_bokeh_project/settings.py
+++ blur_bokeh_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,13 +86,26 @@ WSGI_APPLICATION = "blur_bokeh_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+ENVIRONMENT = config('ENVIRONMENT', default='local')
+SECRET_KEY = config('SECRET_KEY')
+if ENVIRONMENT == 'local':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DATABASE_NAME'),
+            'USER': config('DATABASE_USER'),
+            'PASSWORD': config('DATABASE_PASSWORD'),
+            'HOST': config('DATABASE_HOST'),
+            'PORT': config('DATABASE_PORT'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
