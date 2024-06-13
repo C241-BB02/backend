ba2b9651fc38a60245b730a18df5015c19438a2a
Gibrano Derenz <gibranoderenz123@gmail.com>
2024-06-13 21:50:47 +0700

    feat: add cors allowed origins

--- blur_bokeh_project/settings.py
+++ blur_bokeh_project/settings.py
@@ -36,7 +36,9 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 #TODO: FOR DEV purposes only
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://blurredbasket.vercel.app/',
+]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = False
